4a02a854966b22ab3ef8f8a5dfff55375ee50c1b
Bi0informatic <147007881+Bi0informatic@users.noreply.github.com>
2025-07-28 23:16:03 -0700

    added function that removes all non-alphanumeric characters from string and added even listeners to button and input

--- script.js
+++ script.js
@@ -1,12 +1,28 @@
 const textInput = document.getElementById("text-input");
+const checkButton = document.getElementById("check-btn");
 
-// alerts page if input is empty
-function invalidInput() {
-    if (!textInput.value) {
+//alerts page if input is empty
+function ifInvalidInput() {
+    if (!cleanInput(textInput.value)) {
         alert("Please input a value");
+        return;
     }
 }
 
-function cleanInput() {
-    
+//removes all non-alphanumeric characters (punctation, spaces, and symbols), turns every character to lower
+function cleanInput(str) {
+    return str.replace(/[^a-zA-Z0-9]/g, '');
 }
+
+//checks if valid then if palindrome
+function onSubmit() {
+    ifInvalidInput(); 
+}
+
+checkButton.addEventListener("click", onSubmit);
+textInput.addEventListener("keypress", (e)=>{
+    if (e.key === "Enter") {
+        e.preventDefault();
+        checkButton.click();
+    }
+})
