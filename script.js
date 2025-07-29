110690b310456c7c9230ac2a2a19d2e05ba9f1d2
Bi0informatic <147007881+Bi0informatic@users.noreply.github.com>
2025-07-28 22:56:22 -0700

    added an input for palindrome and button to submit, and alert when button is clicked and there is nothing

---
+++ script.js
@@ -0,0 +1,12 @@
+const textInput = document.getElementById("text-input");
+
+// alerts page if input is empty
+function invalidInput() {
+    if (!textInput.value) {
+        alert("Please input a value");
+    }
+}
+
+function cleanInput() {
+    
+}
